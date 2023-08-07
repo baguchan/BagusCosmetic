83e7cd369d3b7d09740ccf088c9291ae3e750e3e
baguchan <shutux500@gmail.com>
2023-08-07 21:22:19 +0900

    1.19.2-0.1.3

--- src/main/java/baguchan/bagus_cosmetic/item/CosmeticItem.java
+++ src/main/java/baguchan/bagus_cosmetic/item/CosmeticItem.java
@@ -1,26 +1,42 @@
 package baguchan.bagus_cosmetic.item;
 
 import baguchan.bagus_cosmetic.util.CosmeticUtils;
+import com.google.common.collect.LinkedHashMultimap;
+import com.google.common.collect.Multimap;
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
+import top.theillusivec4.curios.api.CuriosApi;
+import top.theillusivec4.curios.api.SlotContext;
 import top.theillusivec4.curios.api.type.capability.ICurioItem;
 
 import javax.annotation.Nullable;
 import java.util.List;
+import java.util.UUID;
 
 public class CosmeticItem extends Item implements ICurioItem {
-    public CosmeticItem(Item.Properties properties) {
-        super(properties);
-    }
+    public final String slotType;
 
+    public CosmeticItem(Properties properties, String slotType) {
+        super(properties);
+        this.slotType = slotType;
+    }
     public void appendHoverText(ItemStack p_42880_, @Nullable Level p_42881_, List<Component> p_42882_, TooltipFlag p_42883_) {
         if (p_42880_.getTag() != null && p_42880_.getTag().contains("Cosmetic")) {
             p_42882_.add(Component.literal("Cosmetic ID :").withStyle(ChatFormatting.DARK_AQUA));
             p_42882_.add(Component.literal(CosmeticUtils.cosmeticLocation(p_42880_).toString()));
         }
+    }
+
+    @Override
+    public Multimap<Attribute, AttributeModifier> getAttributeModifiers(SlotContext slotContext, UUID uuid, ItemStack stack) {
+        Multimap<Attribute, AttributeModifier> atts = LinkedHashMultimap.create();
+        CuriosApi.getCuriosHelper().addSlotModifier(atts, slotType, uuid, 1.0, AttributeModifier.Operation.ADDITION);
+        return atts;
     }
 }
