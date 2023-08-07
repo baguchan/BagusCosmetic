51e3af26b2ce1035a007a1536645eae049857e6c
baguchan <shutux500@gmail.com>
2023-08-07 21:18:52 +0900

    slot increase

--- src/main/java/baguchan/bagus_cosmetic/registry/ModItems.java
+++ src/main/java/baguchan/bagus_cosmetic/registry/ModItems.java
@@ -6,12 +6,13 @@ import net.minecraft.world.item.Item;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
+import top.theillusivec4.curios.api.SlotTypePreset;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, BagusCosmetic.MODID);
 
-    public static final RegistryObject<Item> COSMETIC_BACK = ITEMS.register("cosmetic_back", () -> new CosmeticItem((new Item.Properties())));
-    public static final RegistryObject<Item> COSMETIC_HEAD = ITEMS.register("cosmetic_head", () -> new CosmeticItem((new Item.Properties())));
-    public static final RegistryObject<Item> COSMETIC_BODY = ITEMS.register("cosmetic_body", () -> new CosmeticItem((new Item.Properties())));
+    public static final RegistryObject<Item> COSMETIC_BACK = ITEMS.register("cosmetic_back", () -> new CosmeticItem((new Item.Properties()), SlotTypePreset.BACK.getIdentifier()));
+    public static final RegistryObject<Item> COSMETIC_HEAD = ITEMS.register("cosmetic_head", () -> new CosmeticItem((new Item.Properties()), SlotTypePreset.HEAD.getIdentifier()));
+    public static final RegistryObject<Item> COSMETIC_BODY = ITEMS.register("cosmetic_body", () -> new CosmeticItem((new Item.Properties()), SlotTypePreset.BODY.getIdentifier()));
 
 }
