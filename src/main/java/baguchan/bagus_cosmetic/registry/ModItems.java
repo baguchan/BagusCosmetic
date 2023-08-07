cf7d4dfd2d004389662d61d416c31ac342e85dea
baguchan <shutux500@gmail.com>
2023-08-07 21:19:57 +0900

    slot increase

--- src/main/java/baguchan/bagus_cosmetic/registry/ModItems.java
+++ src/main/java/baguchan/bagus_cosmetic/registry/ModItems.java
@@ -11,8 +11,8 @@ import top.theillusivec4.curios.api.SlotTypePreset;
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, BagusCosmetic.MODID);
 
-    public static final RegistryObject<Item> COSMETIC_BACK = ITEMS.register("cosmetic_back", () -> new CosmeticItem((new Item.Properties()), SlotTypePreset.BACK.getIdentifier()));
-    public static final RegistryObject<Item> COSMETIC_HEAD = ITEMS.register("cosmetic_head", () -> new CosmeticItem((new Item.Properties()), SlotTypePreset.HEAD.getIdentifier()));
-    public static final RegistryObject<Item> COSMETIC_BODY = ITEMS.register("cosmetic_body", () -> new CosmeticItem((new Item.Properties()), SlotTypePreset.BODY.getIdentifier()));
+    public static final RegistryObject<Item> COSMETIC_BACK = ITEMS.register("cosmetic_back", () -> new CosmeticItem((new Item.Properties().stacksTo(1)), SlotTypePreset.BACK.getIdentifier()));
+    public static final RegistryObject<Item> COSMETIC_HEAD = ITEMS.register("cosmetic_head", () -> new CosmeticItem((new Item.Properties().stacksTo(1)), SlotTypePreset.HEAD.getIdentifier()));
+    public static final RegistryObject<Item> COSMETIC_BODY = ITEMS.register("cosmetic_body", () -> new CosmeticItem((new Item.Properties().stacksTo(1)), SlotTypePreset.BODY.getIdentifier()));
 
 }
